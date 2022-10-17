ee97171e36159d4fb005bab4734af6e165de4313
Syzer209 <113340696+Syzer209@users.noreply.github.com>
2022-10-17 11:39:24 +0200

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,6 @@
+password = "contraseña"
+user_password = input("Intrduce la contraseña: \n")
+if user_password.lower() == password.lower()
+    print("La contraseña es Correcta")
+else:
+    print("La contraseña es Erronea")
