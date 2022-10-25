78c81b1eb2e8938e3641f98359749d7973c5aa04
Leyre <113433045+lgarciarob@users.noreply.github.com>
2022-10-25 13:41:36 +0200

    Ejercicio10

---
+++ Ejercicio10.py
@@ -0,0 +1,5 @@
+contraseña = input("Introduzca su contraseña: ")
+while contraseña != "llave":
+   print("Su contraseña es incorrecta.")
+   contraseña = input("Introduzca su contraseña: ")
+print("Su contraseña es correcta.")
