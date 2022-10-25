9708df8ac69ed00ff460fd1686164c920db6598a
Syzer209 <113340696+Syzer209@users.noreply.github.com>
2022-10-25 11:31:18 +0200

    Ejercicio5

---
+++ Ejercicio5.py
@@ -0,0 +1,19 @@
+#Los alumnos de un curso se han dividido en dos grupos A y B de acuerdo al sexo y el nombre.
+# El grupo A está formado por las mujeres con un nombre anterior a la M y los hombres con un nombre posterior a la N y el grupo B por el resto.
+# Escribir un programa que pregunte al usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
+nombre = input("Introduce un nombre: ")
+sexo = input("Introduce el sexo (H o M): ")
+
+if sexo == "M":
+    if nombre[0].lower() < "m":
+        print("Grupo A")
+
+    else:
+        print("Grupo B")
+
+else:
+    if nombre[0].lower() < "n":
+        print("Grupo B")
+
+    else:
+        print("Grupo A")
